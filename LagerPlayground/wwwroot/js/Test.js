1f37e86a00bc24caef0cbdc576cbaa2671c26679
Jakob <johnaasc@hotmail.com>
2022-01-05 18:34:41 +0100

    Scanner Update

---
+++ LagerPlayground/wwwroot/js/Test.js
@@ -0,0 +1,52 @@
+﻿
+var barcode = '';
+var interval;
+document.addEventListener("keydown", function (evt) {
+    if (interval) {
+        clearInterval(interval);
+    }
+    // if input is enter
+    if (evt.code.includes("Enter")) {
+        if (barcode) {
+            handleBarcode(barcode);
+            barcode = '';
+            return;
+        }
+    }
+    if (!evt.code.includes("Shift")) {
+        barcode += evt.key;
+    }
+    interval = setInterval(() => barcode = '', 18);
+});
+
+function handleBarcode(scanned_barcode) {
+    if (scanned_barcode.length >= 1) {
+
+        /*        console.log(scanned_barcode);*/
+
+        $(document).ready(function () {
+            $.ajax({
+                type: "GET",
+                url: "/Product/GetJson",
+                data: { productID: scanned_barcode },
+                dataType: "json",
+                success: function (msg) {
+                    if (msg != false) {
+                        /*console.log(msg);*/
+                        postHtml(msg);
+                    }
+                },
+                error: function (req, status, error) {
+                    Console.log(error);
+                }
+            });
+        });
+    }
+}
+
+function postHtml(data) {
+    var table = document.getElementById("Table");
+
+    $(table).append('<tr><td>' + data.name + '</td><td>' + data.productID + '</td><td>' + data.brandName + '</td><td>' + data.quantity + '</td><tr/>');
+
+}
