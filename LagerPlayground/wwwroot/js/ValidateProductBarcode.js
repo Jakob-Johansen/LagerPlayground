4e7902208ed9ed16f5ad813c2804a3d5b93916f8
Jakob <SpAyS@users.noreply.github.com>
2022-01-07 16:31:15 +0100

    Update

--- LagerPlayground/wwwroot/js/ValidateProductBarcode.js
+++ LagerPlayground/wwwroot/js/ValidateProductBarcode.js
@@ -1,11 +1,11 @@
-﻿
-
-
-var barcode = '';
+﻿var barcode = '';
 var interval;
 
 var errorTextOutputTag = $(".create-overlay-modal-error-text");
 var errorIconOutputTag = $(".create-overlay-modal-error-icon");
+var submitBtn = $(".create-overlay-modal-enter-btn");
+var modalInput = $(".create-overlay-modal-input");
+var modalInputValue = modalInput.val().trim();
 var loader = $(".loader");
 var result = false;
 
@@ -27,50 +27,29 @@ document.addEventListener("keydown", function (evt) {
         }
         interval = setInterval(() => barcode = '', 20);
     }
+
+    if (modalInput.is(":focus") && evt.code.includes("Enter") && modalInput.val().trim().length != 0) {
+        ajaxCall(modalInput.val().trim())
+        loadingTimeout();
+    }
 });
 
-function handleBarcode(scanned_barcode) {
-    if (scanned_barcode.length >= 1) {
-
-        errorTextOutputTag.text("");
-        errorIconOutputTag.text("");
-
-        setTimeout(function () {
-            if (result == false) {
-                loader.css("display", "block");
-            }
-        }, 20)
-
-        console.log(scanned_barcode);
-        $(".inputTest").val(scanned_barcode);
-
-        $.ajax({
-            type: "GET",
-            url: "/Scanner/BarcodeExist",
-            data: { productID: scanned_barcode },
-            dataType: "json",
-            success: function (dataResult) {
-                result = true;
-                loader.css("display", "none");
-                if (dataResult != false) {
-                    errorTextIconColor("#dc3545");
-                    errorTextOutputTag.text("This product already exist");
-                    errorIconOutputTag.text("error_outline");
-                }
-                else {
-                    errorTextIconColor("#28a745")
-                    errorTextOutputTag.text("This product does not exist");
-                    errorIconOutputTag.text("check_circle_outline");
-                }
-            },
-            error: function (req, status, error) {
-                Console.log(error);
-                console.log("Error")
-            }
-        })
+// When typing in the input field.
+modalInput.on('input', function () {
+    if (!loader.is(":visible")) {
+        errorTextIconClearText();
+        submitBtn.css("display", "block");
     }
-}
+});
 
+submitBtn.click(function () {
+    if (modalInput.val().trim().length != 0) {
+        ajaxCall(modalInput.val().trim());
+        loadingTimeout();
+    }
+});
+
+// Click function that close the Modal.
 $(".create-overlay-modal-close-btn").click(function () {
     $(".create-overlay-container").css("display", "none");
 });
@@ -78,4 +57,61 @@ $(".create-overlay-modal-close-btn").click(function () {
 function errorTextIconColor(color) {
     errorTextOutputTag.css("color", color);
     errorIconOutputTag.css("color", color);
+}
+
+function errorTextIconClearText() {
+    errorTextOutputTag.text("");
+    errorIconOutputTag.text("");
+}
+
+function loadingTimeout() {
+    submitBtn.css("display", "none");
+    setTimeout(function () {
+        if (result == false) {
+            loader.css("display", "block");
+        }
+    }, 20);
+}
+
+// Barcode scanner function
+function handleBarcode(scanned_barcode) {
+    scanned_barcode = scanned_barcode.trim();
+    if (scanned_barcode.length >= 1) {
+
+        errorTextIconClearText();
+        loadingTimeout();
+
+        //console.log(scanned_barcode);
+        modalInput.val(scanned_barcode);
+
+        ajaxCall(scanned_barcode);
+    }
+}
+
+// Ajax call function
+function ajaxCall(barcode) {
+    $.ajax({
+        type: "GET",
+        url: "/Scanner/BarcodeExist",
+        data: { productID: barcode },
+        dataType: "json",
+        success: function (dataResult) {
+            result = true;
+            loader.css("display", "none");
+            if (dataResult != false) {
+                errorTextIconColor("#dc3545");
+                errorTextOutputTag.text("This product already exist");
+                errorIconOutputTag.text("error_outline");
+            }
+            else {
+                errorTextIconColor("#28a745")
+                errorTextOutputTag.text("This product does not exist");
+                errorIconOutputTag.text("check_circle_outline");
+            }
+        },
+        error: function (req, status, error) {
+            Console.log(error);
+            console.log("Error")
+        }
+    });
 }
