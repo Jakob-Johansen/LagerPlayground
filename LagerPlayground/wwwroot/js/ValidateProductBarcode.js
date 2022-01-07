76a0259154d50a6fe663c59fb95f0ec2a07a479d
Jakob <SpAyS@users.noreply.github.com>
2022-01-07 12:05:34 +0100

    Create modal Update.

--- LagerPlayground/wwwroot/js/ValidateProductBarcode.js
+++ LagerPlayground/wwwroot/js/ValidateProductBarcode.js
@@ -3,8 +3,13 @@
 
 var barcode = '';
 var interval;
+
+var errorOutputTag = $(".create-overlay-modal-error-text");
+
 document.addEventListener("keydown", function (evt) {
     if ($(".create-overlay-container").is(":visible")) {
+        errorOutputTag.text("");
+
         if (interval) {
             clearInterval(interval);
         }
@@ -27,6 +32,23 @@ function handleBarcode(scanned_barcode) {
     if (scanned_barcode.length >= 1) {
         console.log(scanned_barcode);
         $(".inputTest").val(scanned_barcode);
+
+        $.ajax({
+            type: "GET",
+            url: "/Scanner/BarcodeExist",
+            data: { productID: scanned_barcode },
+            dataType: "json",
+            success: function (dataResult) {
+                if (dataResult != false) {
+                    console.log("This product already exist");
+                    errorOutputTag.text("This product already exist");
+                }
+            },
+            error: function (req, status, error) {
+                Console.log(error);
+                console.log("Error")
+            }
+        })
     }
 }
 
