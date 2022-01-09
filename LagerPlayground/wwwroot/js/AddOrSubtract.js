0ec560323e5c1ab5646e1afed6aea06347bfd206
Jakob <SpAyS@users.noreply.github.com>
2022-01-09 14:14:24 +0100

    Update

---
+++ LagerPlayground/wwwroot/js/AddOrSubtract.js
@@ -0,0 +1,11 @@
+﻿const addBtn = $(".addOrSubtract-add-btn");
+const subtractBtn = $(".addOrSubtract-subtract-btn");
+const inputField = $(".addOrSubtract-input");
+
+addBtn.click(function () {
+	console.log("Add");
+});
+
+subtractBtn.click(function () {
+	console.log("Substract");
+});
