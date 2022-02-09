4acb144a5648fa80ef60071224a30a566634838c
Jakob <johnaasc@hotmail.com>
2022-02-09 23:13:18 +0100

    Update

---
+++ LagerPlayground/Migrations/20220209221224_ReceivingBoxItemsCreate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LagerPlayground.Migrations
+{
+    public partial class ReceivingBoxItemsCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReceiveBox_Items",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReceiveBoxID = table.Column<int>(type: "int", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ReceivingBoxID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiveBox_Items", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ReceiveBox_Items_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReceiveBox_Items_ReceivingBoxes_ReceivingBoxID",
+                        column: x => x.ReceivingBoxID,
+                        principalTable: "ReceivingBoxes",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiveBox_Items_ProductID",
+                table: "ReceiveBox_Items",
+                column: "ProductID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiveBox_Items_ReceivingBoxID",
+                table: "ReceiveBox_Items",
+                column: "ReceivingBoxID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReceiveBox_Items");
+        }
+    }
+}
---
+++ LagerPlayground/Models/ReceiveBox_Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerPlayground.Models
+{
+    public class ReceiveBox_Items
+    {
+        public int ID { get; set; }
+        public int ReceiveBoxID { get; set; }
+        public int ProductID { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+
+        public ReceivingBox ReceivingBox { get; set; }
+        public Product Product { get; set; }
+    }
+}
