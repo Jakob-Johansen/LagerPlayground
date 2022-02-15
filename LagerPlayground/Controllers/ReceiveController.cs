2f76387b3554139789c62c5984c775a69591d5d0
Jakob <SpAyS@users.noreply.github.com>
2022-02-15 09:54:18 +0100

    Update

--- LagerPlayground/Controllers/ReceiveController.cs
+++ LagerPlayground/Controllers/ReceiveController.cs
@@ -1,4 +1,5 @@
 ﻿using LagerPlayground.Data;
+using LagerPlayground.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -30,6 +31,42 @@ namespace LagerPlayground.Controllers
             }
 
             return View(receiveOrderDetails);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<JsonResult> AddProduct(string barcode, int receivingOrderDetailsID)
+        {
+            if (barcode == null)
+            {
+                return Json(new {boolean = false, msg = "No barcode found"});
+            }
+
+            var productToUpdate = await _context.Products.FirstOrDefaultAsync(x => x.BarcodeID == barcode);
+
+            if (productToUpdate == null)
+            {
+                return Json(new {boolean = false, msg = "No product with this barcode was found"});
+            }
+
+            int updatedQuantity = productToUpdate.Quantity + 1;
+
+            if (await TryUpdateModelAsync<Product>(
+                productToUpdate,
+                "",
+                p => p.Quantity))
+            {
+                try
+                {
+                    productToUpdate.Quantity = updatedQuantity;
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    return Json(new { boolean = false, msg = "An database error has occured" });
+                }
+            }
+            return Json(new {boolean = true});
         }
     }
 }
