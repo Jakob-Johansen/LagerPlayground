85388acb4733f8a3e2d23af045d311faabe69e8a
Jakob <SpAyS@users.noreply.github.com>
2022-06-09 12:03:57 +0200

    Update

--- LagerPlayground/Controllers/PickingController.cs
+++ LagerPlayground/Controllers/PickingController.cs
@@ -34,13 +34,63 @@ namespace LagerPlayground.Controllers
                 return Json(new { booleanError = true, msg = "Batch Amount can't be 0" });
             }
 
-            var orders = await _context.Order_Details
-                .Take(numberOfOrders)
+            //var orders = await _context.Order_Details
+            //    .Take(numberOfOrders)
+            //    .Include(x => x.Order_Items)
+            //    .AsNoTracking().ToListAsync();
+
+            var oneOrder = await _context.Order_Details
                 .Include(x => x.Order_Items)
+                .ThenInclude(x => x.Product)
+                .AsNoTracking().FirstOrDefaultAsync();
+
+            var productLocations = await _context.Product_Locations
+                .OrderByDescending(x => x.LocationBarcode)
+                .Where(x => x.LocationBarcode != "Receiving-Station")
                 .AsNoTracking().ToListAsync();
 
+            List<DTOTest> dtoTests = new();
+            foreach (var item in oneOrder.Order_Items)
+            {
+                int orderQuantity = item.Quantity;
+                DTOTest dtoTest = new();
+                dtoTest.Order_DetailsID = item.Order_DetailsID;
+                dtoTest.ProductID = item.ProductID;
+                dtoTest.ProductName = item.Product.Name;
+                dtoTest.ProductBarcode = item.Product.BarcodeID;
 
-            return Json(new { booleanError = false, msg = orders.Count + " orders to pick", orders });
+                foreach (var pLocation in productLocations)
+                {
+                    if(item.ProductID == pLocation.ProductID)
+                    {
+                        // VIRKERR IKKE!
+                        if (orderQuantity >= pLocation.Quantity)
+                        {
+                            orderQuantity -= pLocation.Quantity;
+                            int myTest = item.Quantity - orderQuantity;
+
+                            dtoTest.ProductQuantity = 10;
+                            dtoTest.LocationBarcode = pLocation.LocationBarcode;
+
+                            dtoTests.Add(dtoTest);
+                            if (orderQuantity != 0)
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            dtoTest.ProductQuantity = 20;
+                            dtoTest.LocationBarcode = pLocation.LocationBarcode;
+                            dtoTests.Add(dtoTest);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return Json(new { booleanError = false, msg = 1 + " orders to pick", dtoTests });
+            //return Json(new { booleanError = false, msg = orders.Count + " orders to pick", orders });
         }
     }
 }
---
+++ LagerPlayground/Models/VM/DTOTest.cs
@@ -0,0 +1,12 @@
+﻿namespace LagerPlayground.Models.VM
+{
+    public class DTOTest
+    {
+        public int Order_DetailsID { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+        public string ProductBarcode { get; set; }
+        public string LocationBarcode { get; set; }
+    }
+}
