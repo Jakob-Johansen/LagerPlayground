c9d5a2f518d5205fc36f0a72b034588c3f0bcd80
Jakob <SpAyS@users.noreply.github.com>
2022-06-28 11:59:57 +0200

    Update

---
+++ LagerPlayground/Controllers/PackingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LagerPlayground.Controllers
+{
+    public class PackingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
